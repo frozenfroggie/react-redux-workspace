1fd43476193a94d9b7becb4c3694c476ec2886cb
Jakub Wojtyra <wojtyraj@gmail.com>
2017-10-24 22:44:41 +0200

    more tests added

--- src/redux/reducers/sampleReducer.js
+++ src/redux/reducers/sampleReducer.js
@@ -1,6 +1,8 @@
+import { SAMPLE_ACTION } from '../constants/actionTypes';
+
 const sampleReducer = (state = {sampleInput: "Redux"}, action) => {
   switch(action.type) {
-    case "SAMPLE_ACTION":
+    case SAMPLE_ACTION:
       state = {
         ...state,
         sampleInput: action.payload
--- tests/reducers.spec.js
+++ tests/reducers.spec.js
@@ -1,32 +1,33 @@
 import sampleReducer from "../src/redux/reducers/sampleReducer";
 import secondReducer from "../src/redux/reducers/secondReducer";
+import * as types from '../src/redux/constants/actionTypes';
 
 import chai from "chai";
 var assert = chai.assert;
 var expect = chai.expect;
 
-describe("sampleReducer", function() {
-  describe("if wrong action was provided", function() {
+describe("REDUCERS", function() {
+  describe("sampleReducer", function() {
+    describe("if no action provided and initial state is undefined", function() {
       it('returns default state ', function(){
-       const action = { type: 'UNDEFINED_ACTION', payload: "test" };
-       const newState = sampleReducer(undefined, action);
-       expect(newState).to.deep.equal({sampleInput: "Redux"});
+        const newState = sampleReducer(undefined, {});
+        expect(newState).to.deep.equal({sampleInput: "Redux"});
+      });
      });
-   });
-   describe("if right action was provided", function() {
-    it('returns new state based on given payload', function(){
-     const action = { type: 'SAMPLE_ACTION', payload: "test" };
-     const newState = sampleReducer(undefined, action);
-     expect(newState).to.deep.equal({sampleInput: "test"});
-   });
+     describe("if right action was provided", function() {
+      it('can handle SAMPLE_ACTION', function(){
+        const action = { type: types.SAMPLE_ACTION, payload: "test" };
+        const newState = sampleReducer([], action);
+        expect(newState).to.deep.equal({sampleInput: "test"});
+      });
+    });
   });
-});
-describe("secondReducer", function() {
-  describe("if wrong action was provided", function() {
-    it('returns default state', function(){
-     const action = { type: 'UNDEFINED_ACTION', payload: "test" };
-     const newState = secondReducer(undefined, action);
-     expect(newState).to.deep.equal({secondData: "test"});
+  describe("secondReducer", function() {
+    describe("if no action provided and initial state is undefined", function() {
+      it('returns default state', function(){
+        const newState = secondReducer(undefined, {});
+        expect(newState).to.deep.equal({secondData: "test"});
+      });
     });
   });
 });
